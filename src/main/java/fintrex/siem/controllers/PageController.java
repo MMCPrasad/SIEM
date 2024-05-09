d3d4c1fc173aa2c7a40c5b687ec93175ce2653b1
Chirantha Marasinghe <chirantha.20210210@iit.ac.lk>
2024-05-09 15:47:57 +0530

    dashbaord update commit

---
+++ src/main/java/fintrex/siem/controllers/PageController.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fintrex.siem.controllers;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Chirantha Prasad
+ */
+@Controller
+public class PageController {
+
+    @GetMapping("/Dashboard")
+    public String home() {
+        return "dashbaord";
+    }
+
+//    @GetMapping("/Dashboard-Branch")
+//    public String dashboard() {
+//        return "branch_dashbaord";
+//    }
+//
+//    @GetMapping("/IT-Dashbaord")
+//    public String dashboardIT() {
+//        return "it_dashboard";
+//    }
+//
+//    @GetMapping("/Dashbaord-Approver")
+//    public String dashboardApprover() {
+//        return "approver_dashboard";
+//    }
+//
+//    @GetMapping("/users")
+//    public String users() {
+//        return "users";
+//    }
+//
+//    @GetMapping("/login")
+//    public String login() {
+//        return "login";
+//    }
+//
+//    @GetMapping("/index")
+//    public String viewDashboard(HttpSession session) {
+//        String userType = session.getAttribute("usertype") + "";
+//
+//        if (userType != null) {
+//            switch (userType) {
+//                case "1":
+//                    return "it_dashboard";
+//                case "2":
+//                    return "approver_dashboard";
+//                case "3":
+//                    return "branch_dashbaord";
+//                case "4":
+//                    return "admin_dashboard";
+//                default:
+//                    return "login";
+//            }
+//        } else {
+//
+//            return "login";
+//        }
+//    }
+//
+//    @GetMapping("/")
+//    public String index(HttpSession session) {
+//        String userType = (String) session.getAttribute("usertype");
+//
+//        if (userType != null) {
+//            switch (userType) {
+//                case "1":
+//                    return "it_dashboard";
+//                case "2":
+//                    return "approver_dashboard";
+//                case "3":
+//                    return "branch_dashbaord";
+//                case "4":
+//                    return "admin_dashboard";
+//                default:
+//                    return "login";
+//            }
+//        } else {
+//
+//            return "login";
+//        }
+//    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/dashbaord.jsp
@@ -0,0 +1,26 @@
+<%-- 
+    Document   : dashbaord
+    Created on : 9 May 2024, 1:54:07 pm
+    Author     : Chirantha Prasad
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Fintrex - SIEM</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/css/bootstrap-extended.min.css">
+        <link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/fonts/simple-line-icons/style.min.css">
+        <link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/css/colors.min.css">
+        <link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <%@include file="jspf/header.jspf" %>
+    </head>
+    <body>
+        <%@include file="jspf/navbar.jspf" %>
+    </body>
+</html>
